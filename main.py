339e1ff57724bddd6f30fa14c2e572e847beddd3
vovan4ok <bosik_ck@ukr.net>
2023-11-29 19:40:56 +0300

    make changes

--- main.py
+++ main.py
@@ -16,12 +16,11 @@ lost_money_per_every_day = []
 translated_pages_per_every_day = []
 max_pages = speed * hours * number_of_translators
 burnt_pages_per_every_day = []
-days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+days_of_week = ["monday", "tuesday", "wednesday", "thursday", "friday"]
 speed_per_every_day = []
 
 
 def get_docs_number_for_the_day(day):
-    docs_number = 0
     if day == "monday":
         docs_number = np.random.randint(2, 4)
     elif day == "tuesday":
@@ -39,14 +38,18 @@ def model_immitation():
     for i in range(modeling_durability):
         day = days_of_week[i % 5]
         docs_number = get_docs_number_for_the_day(day)
-        total_pages_for_this_day = 0
+        total_pages_for_this_day = docs_number * np.random.poisson(math_hope_pages_per_doc)
 
-        if np.random.rand() >= bad_feel_possibility:
-            total_pages_for_this_day = docs_number * np.random.poisson(math_hope_pages_per_doc)
+        if np.random.rand() <= bad_feel_possibility:
+            total_pages_copy = total_pages_for_this_day
+            total_pages_for_this_day = 0
 
         if total_pages_for_this_day < max_pages:
             translated_pages_for_this_day = total_pages_for_this_day
-            burnt_pages_per_every_day.append(0)
+            if total_pages_for_this_day == 0:
+                burnt_pages_per_every_day.append(total_pages_copy)
+            else:
+                burnt_pages_per_every_day.append(0)
         else:
             translated_pages_for_this_day = max_pages
             burnt_pages_per_every_day.append(total_pages_for_this_day - translated_pages_for_this_day)
@@ -59,8 +62,11 @@ def model_immitation():
         print("####################################################################################################################")
         print("Номер ітерації: " + str(i))
         print("День тижня: " + day)
-        print("Кількість документів, які надйшли на переклад: " + str(docs_number))
-        print("Загальна кількість сторінок: " + str(total_pages_for_this_day))
+        print("Кількість документів, які надійшли на переклад: " + str(docs_number))
+        if total_pages_for_this_day == 0:
+            print("Загальна кількість сторінок: " + str(total_pages_copy))
+        else:
+            print("Загальна кількість сторінок: " + str(total_pages_for_this_day))
         print("Кількість сторінок, які зміг перекласти перекладач відповідно до свєї максимальної шквидкості: " + str(translated_pages_per_every_day[i]))
         print("Було принесено коштів компанії за поточний день: " + str(earned_money_per_every_day[i]))
         print("Кількість сторінок, яка 'згоріла' через те, що швидкість перекладача обмежена: " + str(burnt_pages_per_every_day[i]))
@@ -72,10 +78,9 @@ def model_immitation():
 def build_histogramma():
     plt.figure(figsize=(13, 5))
     plt.bar(list(range(0, modeling_durability)), translated_pages_per_every_day, color="green", label="Перекладені")
-    plt.bar(list(range(0, modeling_durability)), burnt_pages_per_every_day, color="red", label="Згоріли")
+    plt.bar(list(range(0, modeling_durability)), burnt_pages_per_every_day, color="red", label="Згоріли", bottom=translated_pages_per_every_day)
     plt.xlabel("Ітерація")
     plt.ylabel("Сторінок")
-    plt.ylim((0, 20))
     plt.title("Гістограма співвідношення кількості сторінок по дням")
     plt.legend(loc="upper right")
     plt.show()
